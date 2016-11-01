b430fce6822958565a2f1336ef23234a53694a52
csw <823917541@qq.com>
2016-11-01 15:27:14 +0800

    test

--- src/main/java/com/sedi/controller/UserController.java
+++ src/main/java/com/sedi/controller/UserController.java
@@ -15,20 +15,27 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService userService;
+
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public UserEntity create(@RequestBody(required = true) UserEntity userEntity){
+    public UserEntity create(@RequestBody(required = true) UserEntity userEntity) {
         UserEntity created = userService.createUser(userEntity);
         return created;
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.RESET_CONTENT)
     public UserEntity update(@PathVariable Integer id,
-                                @RequestBody UserEntity user){
-       user.setId(id);
-       UserEntity updated = userService.updateUser(user);
+                             @RequestParam String userName,
+                             @RequestParam String password) {
+        UserEntity user = new UserEntity();
+        user.setId(id);
+        user.setPassword(password);
+        user.setUserName(userName);
+        UserEntity updated = userService.updateUser(user);
         return updated;
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
     public UserEntity delete(@PathVariable Integer id) {
