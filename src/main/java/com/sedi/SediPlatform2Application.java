d8cb1df710788906d4516f5fb39292e22bfbb425
csw <823917541@qq.com>
2016-11-01 09:55:22 +0800

    Initial commit

---
+++ src/main/java/com/sedi/SediPlatform2Application.java
@@ -0,0 +1,12 @@
+package com.sedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SediPlatform2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SediPlatform2Application.class, args);
+	}
+}
