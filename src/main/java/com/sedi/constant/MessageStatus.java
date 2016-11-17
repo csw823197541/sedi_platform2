0531763c22635a12a39f7ce8a9ab45153f891741
lekoxnfx <lekoxnfx@qq.com>
2016-11-17 14:54:49 +0800

    状态

--- src/main/java/com/sedi/constant/MessageStatus.java
+++ src/main/java/com/sedi/constant/MessageStatus.java
@@ -6,6 +6,9 @@ package com.sedi.constant;
  */
 public enum MessageStatus {
 
+    //收到请求->生成报文成功/失败->发送成功/失败
+    //收到报文->解析成功/失败->写入系统成功/部分成功/失败
+
     MS_RECEIVED("已收到"), MS_PARSE_FAILURE("解析失败"), MS_PARSED("已解析"), MS_SEND_FAILURE("发送失败"), MS_SENT("已发送");
 
     private String value;
